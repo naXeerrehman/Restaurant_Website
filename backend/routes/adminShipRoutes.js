ab3fd2680ab5e37d5936688af0013ced923cb0fa
naXeer rehman <naxeerrehman@gmail.com>
2024-12-28 09:19:14 +0000

    Update adminShipRoutes.js

--- backend/routes/adminShipRoutes.js
+++ backend/routes/adminShipRoutes.js
@@ -2,15 +2,11 @@ import express from "express";
 import {
   verifyAdmin,
   getRegisteredUsers,
-  authorizeUser,
 } from "../controllers/adminShip.js";
 
 const router = express.Router();
 
 // Route to get all registered users, accessible only by admins
 router.get("/registered-users", verifyAdmin, getRegisteredUsers);
-
-// Route to authorize a user, accessible only by admins
-router.post("/authorize-user", verifyAdmin, authorizeUser);
 
 export default router;

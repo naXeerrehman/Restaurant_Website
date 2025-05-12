e5aba47c64ade5381bd3f350dc8d6a51bbb4706d
naXeer rehman <naxeerrehman@gmail.com>
2025-05-12 09:56:00 +0500

    Update SignUp.js

--- frontend/src/components/AuthenticationComponents/SignUp.js
+++ frontend/src/components/AuthenticationComponents/SignUp.js
@@ -1,71 +1,70 @@
-import { useEffect, useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import React, { useState } from "react";
 import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 import { FaEye, FaRegEyeSlash } from "react-icons/fa";
 import FastFood6 from "../../assets/FastFood6.jpg";
 
-const Login = () => {
+const SignUp = () => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
   const [otp, setOtp] = useState("");
-  const [otpSent, setOtpSent] = useState(false);
-  const [loginLoading, setLoginLoading] = useState(false);
-  const [otpLoading, setOtpLoading] = useState(false);
   const [message, setMessage] = useState("");
+  const [signUpLoading, setSignUpLoading] = useState(false);
+  const [otpLoading, setOtpLoading] = useState(false);
   const [success, setSuccess] = useState(false);
+  const [otpSent, setOtpSent] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
   const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      navigate("/"); // Redirect to homepage if already logged in
-    }
-  }, [navigate]);
-
-  const handleLogin = async (e) => {
+  const handleSignUp = async (e) => {
     e.preventDefault();
+
+    if (password.length < 8) {
+      setMessage("Password must be 8 characters or more.");
+      setSuccess(false);
+      return;
+    }
+
+    const userData = { name, email, password };
+
     try {
-      setLoginLoading(true); // Use login-specific loading
-      const response = await axios.post(`${BACKEND_URL}/api/auth/login`, {
-        email,
-        password,
-      });
+      setSignUpLoading(true);
+      const response = await axios.post(
+        `${BACKEND_URL}/api/users/register`,
+        userData
+      );
       setMessage(response.data.message);
+      setOtpSent(true);
       setSuccess(true);
-
-      if (response.data.message.includes("OTP")) {
-        setOtpSent(true);
-      }
-
-      if (response.data.token) {
-        localStorage.setItem("email", email);
-        localStorage.setItem("token", response.data.token);
-      }
     } catch (error) {
-      setMessage(error.response?.data.message || "Error during login process.");
+      setMessage(
+        error.response?.data.message || "Error during registration process."
+      );
       setSuccess(false);
     } finally {
-      setLoginLoading(false);
+      setSignUpLoading(false); // Always set loading to false when done
     }
   };
 
   const handleOtpVerify = async (e) => {
     e.preventDefault();
+
     try {
-      setOtpLoading(true); // Use OTP-specific loading
-      const response = await axios.post(`${BACKEND_URL}/api/auth/verify-otp`, {
+      setOtpLoading(true);
+      const response = await axios.post(`${BACKEND_URL}/api/users/verify-otp`, {
         email,
         otp,
       });
+
       localStorage.setItem("token", response.data.token);
       localStorage.setItem("user", JSON.stringify(response.data.user));
       navigate("/");
     } catch (error) {
       setMessage(
-        error.response?.data.message || "Error during OTP verification."
+        error.response?.data.message || "Error during OTP verification process."
       );
       setSuccess(false);
     } finally {
@@ -74,26 +73,43 @@ const Login = () => {
   };
 
   return (
-    <div>
-      {/* Background Image */}
+    <div className="relative min-h-screen">
+     {/* Background Image */}
       <div className="absolute w-full">
         <img src={FastFood6} alt="FastFood_image" className="w-full h-screen" />
       </div>
+
       {/* Content */}
-      <div className="relative inset-0 flex items-center justify-center">
+     <div className="relative inset-0 flex items-center justify-center pt-20 md:pt-44 lg:pt-10">
         {!otpSent ? (
-          <form
-            onSubmit={handleLogin}
-            className="mx-4 w-full p-6 px-6 pt-[80px] md:px-28 lg:px-[370px]"
-          >
-            <h1 className="text-2xl font-bold bg-red-600 text-white rounded-md py-1 text-center mb-6">
-              Login
+          <form onSubmit={handleSignUp} className="w-full max-w-md p-8">
+            <h1 className="text-2xl font-bold bg-red-600 text-white rounded-md py-2 text-center mb-6">
+              Sign Up
             </h1>
 
             <div className="mb-4">
               <label
+                htmlFor="name"
+                className="block text-sm font-bold text-white md:text-2xl"
+              >
+                Name
+              </label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                placeholder="Enter Your Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-red-600"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
                 htmlFor="email"
-                className="block text-sm md:text-2xl font-bold text-white"
+                className="block text-sm font-bold text-white md:text-2xl"
               >
                 Email
               </label>
@@ -105,14 +121,14 @@ const Login = () => {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 text-black rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-red-600"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-red-600"
               />
             </div>
 
             <div className="mb-4">
               <label
                 htmlFor="password"
-                className="block text-sm md:text-2xl font-bold text-white"
+                className="block text-sm font-bold text-white md:text-2xl"
               >
                 Password
               </label>
@@ -125,57 +141,52 @@ const Login = () => {
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   required
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 text-black rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-red-600"
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-red-600"
                 />
                 <button
                   type="button"
                   onClick={() => setShowPassword((prev) => !prev)}
-                  className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5 text-black"
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500"
                 >
                   {showPassword ? <FaEye /> : <FaRegEyeSlash />}
                 </button>
               </div>
             </div>
 
-            <div className="text-sm text-gray-600 mb-4">
-              <Link
-                to="/ResetPassword"
-                className="font-semibold text-white md:text-2xl"
-              >
-                Forgot Password?
-              </Link>
-            </div>
-
-            <div className="text-sm md:text-2xl text-white mb-6">
-              Don't have an account?{" "}
-              <Link to="/SignUpPage" className="font-semibold text-white">
-                Sign up here!
+            <div className="text-sm text-white mb-4 md:text-2xl">
+              Already have an account?{" "}
+              <Link to="/loginPage" className="font-semibold text-white">
+                Log in here!
               </Link>
             </div>
 
             {message && (
-              <p className={`text-center text-2xl mb-1 text-white bg-red-600`}>
+              <p
+                className={`text-center text-sm py-2 ${
+                  success ? "text-green-600" : "text-red-600"
+                }`}
+              >
                 {message}
               </p>
             )}
 
             <button
               type="submit"
-              disabled={loginLoading}
-              className={`w-full font-bold bg-red-600 text-white py-2 px-4 rounded-md hover:bg-red-700 md:text-2xl${
-                loginLoading ? "opacity-50 cursor-not-allowed" : ""
+              disabled={signUpLoading}
+              className={`w-full font-bold bg-red-600 text-white py-2 px-4 rounded-md hover:bg-red-700 transition-colors ${
+                signUpLoading ? "opacity-70" : ""
               }`}
             >
-              {loginLoading ? "Logging in..." : "Click To Login"}
+              {signUpLoading ? "Signing Up..." : "Click To Sign Up"}
             </button>
           </form>
         ) : (
-          <div className="p-6 w-full max-w-md pt-32 md:pt-44">
-            <h2 className="text-2xl font-bold bg-red-600 text-white rounded-md py-1 text-center mb-6">
+          <div className="w-full max-w-md p-8">
+            <h2 className="text-2xl font-bold bg-red-600 text-white rounded-md py-2 text-center mb-6">
               Verify Your OTP
             </h2>
             {message && (
-              <p className={`text-center text-sm bg-red-600 text-white`}>
+              <p className={`text-center text-sm py-2 text-white bg-red-600`}>
                 {message}
               </p>
             )}
@@ -183,7 +194,7 @@ const Login = () => {
               <div>
                 <label
                   htmlFor="otp"
-                  className="block text-sm md:text-2xl text-white font-bold"
+                  className="block text-sm font-bold text-white"
                 >
                   Enter OTP:
                 </label>
@@ -194,15 +205,15 @@ const Login = () => {
                   onChange={(e) => setOtp(e.target.value)}
                   placeholder="Enter OTP"
                   required
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-red-600"
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-red-600"
                 />
               </div>
 
               <button
                 type="submit"
                 disabled={otpLoading}
-                className={`w-full bg-red-600 text-white py-2 px-4 rounded-md hover:bg-red-700 font-bold md:text-2xl ${
-                  otpLoading ? "opacity-50 cursor-not-allowed" : ""
+                className={`w-full bg-red-600 text-white py-2 px-4 rounded-md hover:bg-red-700 font-bold transition-colors ${
+                  otpLoading ? "opacity-70" : ""
                 }`}
               >
                 {otpLoading ? "Verifying OTP..." : "Click To Verify OTP"}
@@ -215,4 +226,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
